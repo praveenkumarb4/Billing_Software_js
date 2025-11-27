46db4adae8449c3b612ff519644b6e1bb661ab22
Praveenkumar <praveenthanadabani@gmail.com>
2025-11-27 12:36:27 +0530

    url change

--- auth.js
+++ auth.js
@@ -1,4 +1,5 @@
-const API_BASE_URL = 'http://localhost:8080/api';
+//const API_BASE_URL = 'http://localhost:8080/api';
+const API_BASE_URL = 'https://billing-software-java.onrender.com/api';
 
 // Check if user is authenticated
 function isAuthenticated() {
